e1849909c50256c731c18b0d02b67f347590ac47
snayderthAes <134348762+snayderthAes@users.noreply.github.com>
2025-01-23 22:53:42 -0500

    Update app.js

--- public/app.js
+++ public/app.js
@@ -2,3 +2,27 @@ function miBoton() {
   alert(`PRONTO DISPONIBLE
 ATT: SNAYDERTH.`);
 }
+
+const btnAll = document.querySelector(".btn-test");
+const registerContainer = document.querySelector(".box-container");
+const btnRegister = document.querySelector(".btn-close");
+const bodyBlack = document.querySelector(".bodyBlack");
+
+btnRegister.addEventListener("click", function () {
+  if (registerContainer.classList.contains("mostrar")) {
+    registerContainer.classList.add("hidden");
+    bodyBlack.classList.contains("mostrar");
+    bodyBlack.classList.add("hidden");
+  }
+});
+
+btnAll.addEventListener("click", function () {
+  if (registerContainer.classList.contains("hidden")) {
+    registerContainer.classList.remove("hidden");
+    bodyBlack.classList.remove("hidden");
+  }
+});
+
+function showElements() {
+  registerContainer;
+}
